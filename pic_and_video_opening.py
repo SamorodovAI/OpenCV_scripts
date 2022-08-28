509cd432c813a02c190b2b594bcf225cd30fdac3
Samorodov <Samorodov_ai@mail.ru>
2022-08-28 13:34:01 +0300

    init

---
+++ pic_and_video_opening.py
@@ -0,0 +1,24 @@
+import cv2
+
+#picture opening
+img = cv2.imread('images/b1.jpg')
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+
+#video opening
+cap = cv2.VideoCapture('videos/B2 Stealth Bomber arrival Fairford (FFD).mp4')
+while True:
+    success, img = cap.read()
+    cv2.imshow('Result', img)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+#camera opening
+cap = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 300)
+while True:
+    success, img = cap.read()
+    cv2.imshow('Result', img)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
